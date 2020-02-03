b5dbb331a0b26ce5df5a88be0c4df9ef1138a829
Gabriel Kaustchr <gabrielkaustchr@gmail.com>
2020-02-03 15:06:04 -0300

    Adicionando Banco a API

--- rotas/produtos.js
+++ rotas/produtos.js
@@ -1,33 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const {Pool} =  require('pg');
+
+const pool = new Pool({
+    host: 'localhost', user: 'postgres', password: '1234', database: 'produtos', port: '5432'
+});
 
 // Retorna todos os produtos
-router.get('/', (req, res, next) =>{
+router.get('/', async(req, res, next) =>{
+    const sql =  await pool.query('SELECT * FROM produto;');
+    res.status(200).json(sql.rows);
     res.status(200).send({
         mensagem: 'SELECT realizado com Sucesso'
     });
 });
 
 //INSERE um Produto
-router.post('/', (req, res, next) =>{
+router.post('/', async(req, res, next) =>{
+    const produto = {
+        nome: req.body.nome,
+        preco: req.body.preco
+    };
+    const sql = await pool.query('INSERT INTO produto ( nome, preco) VALUES( $1, $2)' ,  [ produto.nome, produto.preco]);
     res.status(201).send({
-        mensagem: 'INSERIR realizado com Sucesso'
+        mensagem: 'INSERIR realizado com Sucesso',
+        insert: produto
     });
 });
 
 //Retorna um produto pelo ID
-router.get('/:id_produto', (req, res, next) => {
+router.get('/:id_produto', async(req, res, next) => {
     const id = req.params.id_produto;
-    if(id === 'especial'){
-        res.status(200).send({
-            mensagem: 'Você descobriu o id Especial',
-            id: id
-        });    
-    }else{
-        res.status(200).send({
-            mensagem: 'Usando o GET de um produto Exclusivo',
-            id: id
+    const sql = await pool.query('SELECT * FROM produto WHERE id = $1', [id]);
+    if(sql.rows != ""){
+        res.status(201).json({
+            Produto: sql.rows
+
         });
+        console.log(sql.rows);
+    }else{
+        res.status(404).send({
+            Mensagem: 'Produto não encontrado'
+        })
     }
 });
 
